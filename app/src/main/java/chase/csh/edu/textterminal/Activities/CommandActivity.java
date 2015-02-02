b7a3cbfac6af7de43b01f28f43ae19a48b98d9a7
chase <chase@csh.rit.edu>
2015-02-02 16:58:04 -0500

    Started work on the adapters and activity for the command Activity

--- app/src/main/java/chase/csh/edu/textterminal/Activities/CommandActivity.java
+++ app/src/main/java/chase/csh/edu/textterminal/Activities/CommandActivity.java
@@ -1,12 +1,23 @@
 package chase.csh.edu.textterminal.Activities;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import chase.csh.edu.textterminal.R;
 
 public class CommandActivity extends TextTerminalActivity {
+
+    private ListView flagListView, parameterListView, extraListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.command_activity_layout);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
 
     }
 }
